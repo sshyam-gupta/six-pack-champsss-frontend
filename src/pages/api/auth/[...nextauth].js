c380eef6f0e86744f5060e9d6d90e7d9dcffa87c
ShubhamJ <shubham@kiprosh.com>
2021-04-12 10:42:51 +0530

    update env changes to handle all hosts

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
-export default NextAuth({
-  // Configure one or more authentication providers here
+
+const options = {
   providers: [
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -16,4 +16,19 @@ export default NextAuth({
       return baseUrl;
     },
   },
-});
+};
+
+export default (req, res) => {
+  setNextAuthUrl(req);
+  NextAuth(req, res, options);
+};
+function setNextAuthUrl(req) {
+  const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+  const host = req.headers['host'];
+
+  if (!host) {
+    throw new Error(`The request has no host header which breaks authentication and authorization.`);
+  }
+
+  process.env.NEXTAUTH_URL = `${protocol}://${host}`;
+}
