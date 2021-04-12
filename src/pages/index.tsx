7666ed3665bdda13640d66a63936b995da772997
ShubhamJ <shubham@kiprosh.com>
2021-04-12 09:33:28 +0530

    create header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
-import { Box, Flex, HStack, Text } from '@chakra-ui/layout';
+import { Box, Flex } from '@chakra-ui/layout';
 import { Spinner } from '@chakra-ui/spinner';
-import { signOut, useSession } from 'next-auth/client';
+import { useSession } from 'next-auth/client';
 import { useRouter } from 'next/dist/client/router';
 import { useEffect } from 'react';
+
+import Header from '../components/Header';
 
 const Index = () => {
   const [session, loading] = useSession();
   const router = useRouter();
-
   useEffect(() => {
     if (!loading && !session) {
       router.push('/auth/signin');
@@ -25,25 +26,7 @@ const Index = () => {
   }
   return (
     <Box>
-      {session && (
-        <>
-          {session.user.image && <span style={{ backgroundImage: `url(${session.user.image})` }} />}
-          <HStack spacing={4} justify="flex-end" p="1rem" bg="gray.100">
-            <Text fontSize="lg" fontWeight={500}>
-              {session.user.email || session.user.name}
-            </Text>
-            <a
-              href={`/api/auth/signout`}
-              onClick={e => {
-                e.preventDefault();
-                signOut();
-              }}
-            >
-              Sign out
-            </a>
-          </HStack>
-        </>
-      )}
+      <Header />
     </Box>
   );
 };
