d344aab358503ea1b27d0167bd34f6df2d2e750b
Shyam Gupta <shyamsundar.g@kiprosh.com>
2021-04-19 10:58:28 +0530

    Adds logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,10 @@
-import { Box, Stack, Text } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 import * as AppData from '../constants/app.json';
 
 export const Footer = () => (
-  <Box as="footer" mt={12} textAlign="center">
+  <Box as="footer" mt={4} p={4} textAlign="center">
     <Text fontSize="sm">
       <span>{`Created with ♥ by ${AppData['presented-by']}`}</span>
     </Text>
-    <Stack mt={4} direction="row" spacing="12px" justify="center"></Stack>
   </Box>
 );
