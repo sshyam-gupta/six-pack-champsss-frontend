f62ba7a09cb847772c80e2929a5c11a19adc75d1
ShubhamJ <shubham@kiprosh.com>
2021-04-25 10:16:33 +0530

    add axios interceptor and handle 401

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,6 +1,32 @@
-import isEmpty from 'lodash/isEmpty';
-import { ApiHeaders } from './headers';
 import axios from 'axios';
+import { signOut } from 'next-auth/client';
+
+axios.interceptors.request.use(
+  config => {
+    //TODO Add token here.
+    const token = '';
+    config.headers = {
+      ...config.headers,
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+    };
+    return {
+      ...config,
+    };
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+axios.interceptors.response.use(
+  res => res,
+  err => {
+    if (err.response.status === 401) {
+      signOut();
+      return;
+    }
+    throw err;
+  },
+);
 
 class ApiService {
   static async post(path: string, params: any = {}, headers?: any) {
@@ -9,16 +35,16 @@ class ApiService {
   static async get(path: string, params: any = {}, headers?: any) {
     return this.xhr(path, params, headers, 'GET');
   }
-  private static async xhr(path: string, params: any = {}, headers: any = {}, method = 'GET') {
+  private static async xhr(path: string, params: any = {}, _headers: any = {}, method = 'GET') {
     const config: any = {
       method,
       data: params,
       baseURL: process.env.NEXT_PUBLIC_BASE_URL,
     };
-    config.headers = isEmpty(headers) ? ApiHeaders.getHeaders() : headers;
     config.url = path;
     config.timeout = 30000;
     let response: any = {};
+
     try {
       response = await axios(config);
     } catch (error) {
